a161926a45c41df8cc0b9f6bf0019224a2fc5473
uyuy06 <acilliitle166@gmail.com>
2022-10-26 04:38:25 +0700

    Post model

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,13 +3,12 @@
 @section('container')
     
     @foreach ($posts as $post)
-    <article class="mb-5">
-        <h2>
-            <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a>
-        </h2>
-        <h5>By: {{ $post["author"] }}</h5>
-        <p>{{ $post["body"] }}</p>
-    </article>
+        <article>
+            <h2>
+                <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+            </h2>
+            <p>{{ $post->excerpt }}</p>
+        </article>
     @endforeach
 
 @endsection
