942417b2e63d344e64be4d08aaa3b2986358d53e
uyuy06 <acilliitle166@gmail.com>
2022-10-25 10:25:22 +0700

    struktur folder view and routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('home');
+});
+
+Route::get('/about', function () {
+    return view ('about',[
+        "name" => "Andi Rahman Hakim",
+        "email" => "andirahman@gmail.com",
+        "image" => "profile.jpeg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view ('posts');
 });
