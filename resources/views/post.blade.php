8096f6542f1cd5be7705d4f902e93115aa5a6e47
uyuy06 <acilliitle166@gmail.com>
2022-10-26 23:12:14 +0700

    Category & Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,8 @@
 @section('container')
         <h1>{{ $post->title }}</h1>
 
+        <p>By. Andi Rahman in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
         {!! $post->body !!}
 
     <a href="/posts">Back to Posts</a>
