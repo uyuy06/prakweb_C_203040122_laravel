7e20812e1ce1fcbfea74bffb34199376764f9f72
uyuy06 <acilliitle166@gmail.com>
2022-10-27 00:44:23 +0700

    Database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +14,65 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // $this->call(UserSeeder::class);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name' => 'Andi Rahman',
+            'email' => 'andiR@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Hakim',
+            'email' => 'hakim@gmail.com',
+            'password' => bcrypt('67890')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia dolore commodi quasi',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia dolore commodi quasi, dolorem ducimus libero quo id pariatur repellendus? Vel eaque quod doloremque distinctio sapiente qui voluptate corporis, nesciunt repudiandae vitae veniam mollitia</p><p> necessitatibus dicta quasi? Animi, voluptatibus. Molestias ut officiis a accusamus. Pariatur, id excepturi autem perferendis in perspiciatis earum consequuntur corporis illo ea, ratione </p><p>voluptatibus totam dicta iure odio ad quibusdam. Magnam temporibus officiis fugiat tempore, incidunt qui quam illo quae illum dolore modi repellat iusto quo deleniti hic itaque impedit quasi dolores distinctio, reprehenderit, sapiente repudiandae. Aliquid, est? Maiores ratione asperiores temporibus dolorem, earum molestiae dolore amet.</p>',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia dolore commodi quasi',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia dolore commodi quasi, dolorem ducimus libero quo id pariatur repellendus? Vel eaque quod doloremque distinctio sapiente qui voluptate corporis, nesciunt repudiandae vitae veniam mollitia</p><p> necessitatibus dicta quasi? Animi, voluptatibus. Molestias ut officiis a accusamus. Pariatur, id excepturi autem perferendis in perspiciatis earum consequuntur corporis illo ea, ratione </p><p>voluptatibus totam dicta iure odio ad quibusdam. Magnam temporibus officiis fugiat tempore, incidunt qui quam illo quae illum dolore modi repellat iusto quo deleniti hic itaque impedit quasi dolores distinctio, reprehenderit, sapiente repudiandae. Aliquid, est? Maiores ratione asperiores temporibus dolorem, earum molestiae dolore amet.</p>',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia dolore commodi quasi',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia dolore commodi quasi, dolorem ducimus libero quo id pariatur repellendus? Vel eaque quod doloremque distinctio sapiente qui voluptate corporis, nesciunt repudiandae vitae veniam mollitia</p><p> necessitatibus dicta quasi? Animi, voluptatibus. Molestias ut officiis a accusamus. Pariatur, id excepturi autem perferendis in perspiciatis earum consequuntur corporis illo ea, ratione </p><p>voluptatibus totam dicta iure odio ad quibusdam. Magnam temporibus officiis fugiat tempore, incidunt qui quam illo quae illum dolore modi repellat iusto quo deleniti hic itaque impedit quasi dolores distinctio, reprehenderit, sapiente repudiandae. Aliquid, est? Maiores ratione asperiores temporibus dolorem, earum molestiae dolore amet.</p>',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia dolore commodi quasi',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia dolore commodi quasi, dolorem ducimus libero quo id pariatur repellendus? Vel eaque quod doloremque distinctio sapiente qui voluptate corporis, nesciunt repudiandae vitae veniam mollitia</p><p> necessitatibus dicta quasi? Animi, voluptatibus. Molestias ut officiis a accusamus. Pariatur, id excepturi autem perferendis in perspiciatis earum consequuntur corporis illo ea, ratione </p><p>voluptatibus totam dicta iure odio ad quibusdam. Magnam temporibus officiis fugiat tempore, incidunt qui quam illo quae illum dolore modi repellat iusto quo deleniti hic itaque impedit quasi dolores distinctio, reprehenderit, sapiente repudiandae. Aliquid, est? Maiores ratione asperiores temporibus dolorem, earum molestiae dolore amet.</p>',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,9 @@
 @section('container')
         <h1>{{ $post->title }}</h1>
 
-        <p>By. Andi Rahman in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By.<a href="#"  class="text-decoration-none" >{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
         {!! $post->body !!}
 
-    <a href="/posts">Back to Posts</a>
+    <a href="/posts" class="d-block mt-3" class="text-decoration-none">Back to Posts</a>
 @endsection
