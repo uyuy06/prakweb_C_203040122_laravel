7509fff7f7845b2fb413a4f6bb28d43a22c5218b
uyuy06 <acilliitle166@gmail.com>
2022-10-31 22:10:30 +0700

    memperbaiki video 11

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.main')
 
 @section('container')
-        <h1>{{ $post->title }}</h1>
+        <h1 class="mb-5">{{ $post->title }}</h1>
 
-        <p>By.<a href="/authors/{{ $post->user->username }}"  class="text-decoration-none" >{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
-
+        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{  $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" href="#" class="text-decoration-none">{{ $post->category->name }}</a></p>
         {!! $post->body !!}
 
-    <a href="/posts" class="d-block mt-3" class="text-decoration-none">Back to Posts</a>
+        <a href="/posts" class="d-block mt-3">Back to Posts</a>
 @endsection
